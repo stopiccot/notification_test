95f19c3149d7247046e0d24f15bcd605221efe8b
stopiccot <alexey.petruchik@gmail.com>
2019-01-30 17:45:55 +0300

    Initial commit

---
+++ Assets/TestScript.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Notifications.iOS;
+
+public class TestScript : MonoBehaviour {
+
+    public async void Click() {
+        using (var req = new AuthorizationRequest(AuthorizationOption.AuthorizationOptionAlert | AuthorizationOption.AuthorizationOptionBadge, true)) {
+            while (!req.IsFinished) {
+                await System.Threading.Tasks.Task.Delay(100);
+            }
+        }
+
+        var date = DateTime.Now.AddMinutes(2);
+        var text = "texttexttext";
+        var title = "titletitletitle";
+
+        var timeTrigger = new iOSNotificationCalendarTrigger {
+            Year = date.Year,
+            Month = date.Month,
+            Day = date.Day,
+            Hour = date.Hour,
+            Minute = date.Minute,
+            Second = date.Second
+        };
+
+        var notification = new iOSNotification {
+            Body = text,
+            ForegroundPresentationOption = PresentationOption.NotificationPresentationOptionAlert,
+            Trigger = timeTrigger
+        };
+
+        if (!String.IsNullOrEmpty(title)) {
+            notification.Title = title;
+        }
+
+        iOSNotificationCenter.ScheduleNotification(notification);
+
+    }
+}
